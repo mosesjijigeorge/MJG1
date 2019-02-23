7ab7f79f938905f637459c51e1b9e48397abfe94
Moses Jiji George <mosesjijigeorge@gmail.com>
2019-02-23 12:49:09 +0530

    PythonFlask1

---
+++ add.py
@@ -0,0 +1,10 @@
+import summ
+
+
+# num1=int(input("Enter 1st Number: "))
+# num2=int(input("Enter 2nd Number: "))
+# s=summ.add(num1,num2)
+# print(s)
+
+print(__name__)
+summ.display()
---
+++ summ.py
@@ -0,0 +1,8 @@
+def display():
+    print(__name__)
+
+
+
+def add(x,y):
+    z=x+y
+    return z
